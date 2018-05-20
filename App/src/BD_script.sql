9d92bfd81738eb88d2df98f6d5dd2d5660cefde4
Eduardo Fuerte <efuerte@uol.com.br>
2018-05-20 17:28:48 -0300

    ajuste 08

--- App/src/BD_script.sql
+++ App/src/BD_script.sql
@@ -22,3 +22,36 @@ AUTO_INCREMENT = 17
 AVG_ROW_LENGTH = 2730
 CHARACTER SET latin1
 COLLATE latin1_swedish_ci;
+
+
+
+SET NAMES 'utf8';
+
+INSERT INTO `5aattiva`.produto(id, nome, preco, quantidade, descricao, dataCadastro, dataInicio, dataFim, idstatus, idsituacao) VALUES
+(12, 'asas', NULL, NULL, 'asas', '2018-05-20 21:19:23', '2017-10-16', '0000-00-00', 2, 2);
+INSERT INTO `5aattiva`.produto(id, nome, preco, quantidade, descricao, dataCadastro, dataInicio, dataFim, idstatus, idsituacao) VALUES
+(13, 'vsvs', NULL, NULL, 'svsv', '2018-05-20 00:35:47', '2017-10-15', '2018-10-28', 3, 2);
+INSERT INTO `5aattiva`.produto(id, nome, preco, quantidade, descricao, dataCadastro, dataInicio, dataFim, idstatus, idsituacao) VALUES
+(14, 'kjkj', NULL, NULL, 'askjkj', '2018-05-19 23:30:50', '2017-10-15', '2018-11-12', 3, 3);
+INSERT INTO `5aattiva`.produto(id, nome, preco, quantidade, descricao, dataCadastro, dataInicio, dataFim, idstatus, idsituacao) VALUES
+(15, 'zcxc', NULL, NULL, 'qwqweq', '2018-05-20 00:16:38', '2017-10-15', '0000-00-00', 1, 2);
+INSERT INTO `5aattiva`.produto(id, nome, preco, quantidade, descricao, dataCadastro, dataInicio, dataFim, idstatus, idsituacao) VALUES
+(16, 'aaaaaaaaaaaa', NULL, NULL, 'aa', '2018-05-20 21:59:42', '2017-10-15', '2017-12-15', 4, 1);
+INSERT INTO `5aattiva`.produto(id, nome, preco, quantidade, descricao, dataCadastro, dataInicio, dataFim, idstatus, idsituacao) VALUES
+(17, 'abc', NULL, NULL, 'abc', '2018-05-20 21:58:26', '2017-10-16', '0000-00-00', 1, 2);
+INSERT INTO `5aattiva`.produto(id, nome, preco, quantidade, descricao, dataCadastro, dataInicio, dataFim, idstatus, idsituacao) VALUES
+(18, 'bbbb', NULL, NULL, 'bbbbb', '2018-05-20 22:17:53', '2017-10-16', '0000-00-00', 4, 1);
+
+
+INSERT INTO `5aattiva`.status(id, nome) VALUES
+(1, 'Pendente');
+INSERT INTO `5aattiva`.status(id, nome) VALUES
+(2, 'Em desenvolvimento');
+INSERT INTO `5aattiva`.status(id, nome) VALUES
+(3, 'Em Teste');
+INSERT INTO `5aattiva`.status(id, nome) VALUES
+(4, 'Concluido');
+
+
+-- Configurar o BD em \App\App.php
+
